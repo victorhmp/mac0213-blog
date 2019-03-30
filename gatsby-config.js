f8989a7f2a45a877295c0632213a25652647986b
Victor Hugo M. Pinto <victor2142@gmail.com>
2019-03-29 22:57:08 -0300

    Initial commit
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

---
+++ gatsby-config.js
@@ -0,0 +1,58 @@
+module.exports = {
+  siteMetadata: {
+    title: 'MAC0213 Blog',
+    author: 'Victor Hugo M. Pinto',
+    description:
+      'A simple blog with updates on activities being conducted as part of the discipline MAC0213',
+    siteUrl: 'https://victorhmp.github.io/mac0213-blog/',
+  },
+  pathPrefix: '/mac0213-blog',
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        //trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    `gatsby-plugin-feed`,
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography',
+      },
+    },
+  ],
+}
